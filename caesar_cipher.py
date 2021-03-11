7b30f303b0ac98216b01ca087e88430fd6f3323b
gk41 <max.hitchings@gmail.com>
2021-03-11 21:13:52 +0000

    added a decrypt function

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -15,12 +15,29 @@ def encrypt(plain_test, shift):
             return f"Sorry i can't handle {char} yet. Should be able to so tho :)"
     return plain_text
 
-def decrypt(cipher_text, shift):
-    #todo
-    pass
+def decrypt(cipher_text):
+    all_possibilities = []
+    for shift in range(26):
+        plain_text = ""
+        for char in cipher_text:
+            char_ascii = ord(char)
+            if char_ascii == 32:
+                plain_text += " "
+            elif char_ascii <= (90-shift) and char_ascii >= 65 or char_ascii <= (122-shift) and char_ascii >= 97:
+                plain_text += chr(char_ascii + shift)
+            elif char.islower() and char_ascii >= (122-shift):
+                plain_text += chr(((char_ascii-(96-shift))%26)+96)
+            elif char.isupper() and char_ascii >= (90-shift):
+                plain_text += chr(((char_ascii-(64-shift))%26)+64)
+            else:
+                return f"Sorry i can't handle {char} yet. Should be able to so tho :)"
+        all_possibilities.append(plain_text)
+    return all_possibilities
+    
 
+#plain_test = input("\nInput your plain text to encrypt: ")
+#shift = int(input("\nWhat do you want the shift to be? "))
+#
+#print(f"\nYour encrypted message is: {encrypt(plain_test, shift)}")
 
-plain_test = input("\nInput your plain text to encrypt: ")
-shift = int(input("\nWhat do you want the shift to be? "))
-
-print(f"\nYour encrypted message is: {encrypt(plain_test, shift)}")
+print(decrypt("z"))
