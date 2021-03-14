c4d637acfed1dc0fffa1faf110f50b337b666047
gk41 <max.hitchings@gmail.com>
2021-03-14 02:36:47 +0000

    Added function 'step' to remove duplicate code from encrypt and decrypt functions

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,14 +1,11 @@
-def encrypt():
-    input_text = input("\nInput your plain text to encrypt: ")
-    shift = int(input("\nWhat do you want the shift to be? "))
+def step(input_text, shift):
     plain_text = ""
     for char in input_text:
         char_ascii = ord(char)
         if char_ascii == 32:
             plain_text += " "
         elif char_ascii <= (90-shift) and char_ascii >= 65 or char_ascii <= (122-shift) and char_ascii >= 97:
-            char_ascii = char_ascii + shift
-            plain_text += chr(char_ascii)
+            plain_text += chr(char_ascii + shift)
         elif char.islower() and char_ascii >= (122-shift):
             plain_text += chr(((char_ascii-(96-shift))%26)+96)
         elif char.isupper() and char_ascii >= (90-shift):
@@ -17,24 +14,17 @@ def encrypt():
             return f"Sorry i can't handle {char} yet. Should be able to so tho :)"
     return plain_text
 
+def encrypt():
+    input_text = input("\nInput your plain text to encrypt: ")
+    shift = int(input("\nWhat do you want the shift to be? "))
+
+    return step(input_text, shift)
+
 def decrypt():
     cipher_text = input("\nInput what you want to decrypt: ")
     all_possibilities = []
     for shift in range(26):
-        plain_text = ""
-        for char in cipher_text:
-            char_ascii = ord(char)
-            if char_ascii == 32:
-                plain_text += " "
-            elif char_ascii <= (90-shift) and char_ascii >= 65 or char_ascii <= (122-shift) and char_ascii >= 97:
-                plain_text += chr(char_ascii + shift)
-            elif char.islower() and char_ascii >= (122-shift):
-                plain_text += chr(((char_ascii-(96-shift))%26)+96)
-            elif char.isupper() and char_ascii >= (90-shift):
-                plain_text += chr(((char_ascii-(64-shift))%26)+64)
-            else:
-                return f"Sorry i can't handle {char} yet. Should be able to so tho :)"
-        all_possibilities.append(plain_text)
+        all_possibilities.append(step(cipher_text, shift))
     return all_possibilities
     
 def user_input():
