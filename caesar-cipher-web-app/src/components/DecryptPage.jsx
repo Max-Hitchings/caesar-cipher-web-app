17d0406d15d1e7dea4bf28bb66af3d9bf6654222
gk41 <max.hitchings@gmail.com>
2021-03-21 00:02:01 +0000

    moved sortResluts into seperate file + fixed bug

--- caesar-cipher-web-app/src/components/DecryptPage.jsx
+++ caesar-cipher-web-app/src/components/DecryptPage.jsx
@@ -2,8 +2,7 @@ import React, { useState, useEffect } from "react";
 import { TextField } from "@material-ui/core";
 import Results from "./Results.jsx";
 import decrypt from "./functions/Decrypt";
-const DetectLanguage = require("detectlanguage");
-const detectlanguage = new DetectLanguage("7644c66e81fea80031848b70140f0f90");
+import sortResults from "./functions/sortResults.js";
 
 export default function DecryptPage() {
   const [textFieldValue, settextFieldValue] = useState("");
@@ -31,39 +30,14 @@ export default function DecryptPage() {
     settextFieldValue(e.target.value);
   };
 
-  const sortResults = async () => {
-    if (!sorted) {
-      setsorted(true);
-      detectlanguage.detect(results).then((response) => {
-        var topResult = { index: null, score: 0 };
-
-        var listIndex;
-        for (listIndex in results) {
-          response[listIndex].map((item, index) => {
-            if (
-              item.language === "en" &&
-              item.isReliable === true &&
-              item.confidence > topResult.score
-            ) {
-              topResult = { index: listIndex, score: item.confidence };
-            }
-            return "";
-          });
-        }
-        if (topResult !== null) {
-          setBestResultValue({
-            index: topResult.index,
-            value: results[topResult.index],
-          });
-        }
-        console.log("Hello", JSON.stringify(response));
-      });
-    }
-  };
-
   const SortButton = () => {
     return (
-      <button className="sortButton" onClick={sortResults}>
+      <button
+        className="sortButton"
+        onClick={() => {
+          sortResults(sorted, setsorted, results, setBestResultValue);
+        }}
+      >
         SORT
       </button>
     );
@@ -94,7 +68,7 @@ export default function DecryptPage() {
           SUBMIT
         </button>
         {results.length > 0 ? <SortButton /> : ""}
-        {BestResultValue.value !== null ? <BestResult /> : ""}
+        {BestResultValue.index !== null || undefined ? <BestResult /> : ""}
         <Results results={results} multiple={true} />
       </div>
     </div>
---
+++ caesar-cipher-web-app/src/components/functions/sortResults.js
@@ -0,0 +1,34 @@
+const DetectLanguage = require("detectlanguage");
+const detectlanguage = new DetectLanguage("7644c66e81fea80031848b70140f0f90");
+
+const sortResults = async (sorted, setsorted, results, setBestResultValue) => {
+  if (!sorted) {
+    setsorted(true);
+    detectlanguage.detect(results).then((response) => {
+      var topResult = { index: null, score: 0 };
+
+      var listIndex;
+      for (listIndex in results) {
+        response[listIndex].map((item, index) => {
+          if (
+            item.language === "en" &&
+            item.isReliable === true &&
+            item.confidence > topResult.score
+          ) {
+            topResult = { index: listIndex, score: item.confidence };
+          }
+          return "";
+        });
+      }
+      if (topResult !== null) {
+        setBestResultValue({
+          index: topResult.index,
+          value: results[topResult.index],
+        });
+      }
+      console.log(JSON.stringify(response));
+    });
+  }
+};
+
+export default sortResults;
