82edd699bb1f146d96a2431ee8f8f7c76fba2dd7
gk41 <max.hitchings@gmail.com>
2021-03-29 00:21:29 +0100

    Made sort function async/await to stop pre fail

--- caesar-cipher-web-app/src/components/functions/sortResults.js
+++ caesar-cipher-web-app/src/components/functions/sortResults.js
@@ -3,31 +3,55 @@ const detectlanguage = new DetectLanguage("7644c66e81fea80031848b70140f0f90");
 
 const sortResults = async (sorted, setsorted, results, setBestResultValue) => {
   if (!sorted) {
-    setsorted(true);
-    detectlanguage.detect(results).then((response) => {
-      var topResult = { index: null, score: 0 };
+    const response = await detectlanguage.detect(results);
+    var topResult = { index: null, score: 0 };
 
-      var listIndex;
-      for (listIndex in results) {
-        response[listIndex].map((item, index) => {
-          if (
-            item.language === "en" &&
-            item.isReliable === true &&
-            item.confidence > topResult.score
-          ) {
-            topResult = { index: listIndex, score: item.confidence };
-          }
-          return "";
-        });
-      }
-      if (topResult !== null) {
-        setBestResultValue({
-          index: topResult.index,
-          value: results[topResult.index],
-        });
-      }
-      console.log(JSON.stringify(response));
-    });
+    var listIndex;
+    for (listIndex in results) {
+      response[listIndex].map((item, index) => {
+        if (
+          item.language === "en" &&
+          item.isReliable === true &&
+          item.confidence > topResult.score
+        ) {
+          topResult = { index: listIndex, score: item.confidence };
+        }
+        return "";
+      });
+    }
+    if (topResult !== null) {
+      setBestResultValue({
+        index: topResult.index,
+        value: results[topResult.index],
+      });
+    }
+    console.log(JSON.stringify(response));
+    setsorted(true);
+
+    // detectlanguage.detect(results).then((response) => {
+    //   var topResult = { index: null, score: 0 };
+
+    //   var listIndex;
+    //   for (listIndex in results) {
+    //     response[listIndex].map((item, index) => {
+    //       if (
+    //         item.language === "en" &&
+    //         item.isReliable === true &&
+    //         item.confidence > topResult.score
+    //       ) {
+    //         topResult = { index: listIndex, score: item.confidence };
+    //       }
+    //       return "";
+    //     });
+    //   }
+    //   if (topResult !== null) {
+    //     await setBestResultValue({
+    //       index: topResult.index,
+    //       value: results[topResult.index],
+    //     });
+    //   }
+    //   console.log(JSON.stringify(response));
+    // });
   }
 };
 
